c3f04061f229ec674077728bf6e1bb6d0591587e
Harshit <workwithvrz@gmail.com>
2025-11-18 20:41:44 +0530

    refactor(db): Refine SQLAlchemy models to strictly enforce 2.x idioms and best practices

--- app/db/models.py
+++ app/db/models.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import Optional, List
 from uuid import uuid4
 from enum import Enum as PyEnum
+from datetime import datetime
 
 from decimal import Decimal 
 from sqlalchemy import (
@@ -47,7 +48,7 @@ class Account(Base):
     currency: Mapped[CurrencyCode] = mapped_column(Enum(CurrencyCode), default=CurrencyCode.INR ,nullable=False)
     balance: Mapped[Decimal] = mapped_column(Numeric(20, 6), nullable=False, default=0)
     status: Mapped[AccountStatus] = mapped_column(Enum(AccountStatus), default=AccountStatus.ACTIVE)
-    created_at: Mapped = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
 
     # Reverse relationships
     sent_transactions: Mapped[List["Transaction"]] = relationship(
@@ -70,7 +71,7 @@ class Transaction(Base):
     __tablename__ = "transactions"
 
     id: Mapped[UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid4)
-    idempotency_key: Mapped[Optional[str]] = mapped_column(String, index=True, nullable=False, unique=True)
+    idempotency_key: Mapped[Optional[str]] = mapped_column(String, index=True, unique=True)
     sender_account: Mapped[UUID] = mapped_column(ForeignKey("accounts.id"))
     receiver_account: Mapped[UUID] = mapped_column(ForeignKey("accounts.id"))
     amount: Mapped[Decimal] = mapped_column(Numeric(20, 6), nullable=False)
@@ -78,9 +79,9 @@ class Transaction(Base):
     status: Mapped[TransactionStatus] = mapped_column(
         Enum(TransactionStatus), default=TransactionStatus.PENDING
     )
-    metadata: Mapped[Optional[dict]] = mapped_column(JSON)
-    created_at: Mapped = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
-    updated_at: Mapped = mapped_column(
+    additional_metadata: Mapped[Optional[dict]] = mapped_column(JSON)
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
         TIMESTAMP(timezone=True),
         server_default=func.now(),
         onupdate=func.now()
@@ -112,7 +113,7 @@ class LedgerEntry(Base):
     entry_type: Mapped[LedgerEntryType] = mapped_column(Enum(LedgerEntryType), nullable=False)  # "DEBIT" or "CREDIT"
     amount: Mapped[Decimal] = mapped_column(Numeric(20, 6), nullable=False)
     balance_after: Mapped[Optional[Decimal]] = mapped_column(Numeric(20, 6))
-    created_at: Mapped = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(TIMESTAMP(timezone=True), server_default=func.now())
 
     # Relationships
     account: Mapped["Account"] = relationship("Account", back_populates="ledger_entries")
