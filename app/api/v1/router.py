8e1e7d5304c1bea87fe38d60c56737d28c573def
Harshit <workwithvrz@gmail.com>
2025-11-20 09:08:34 +0530

    chore(cleanup): Remove temporary test route

--- app/api/v1/router.py
+++ app/api/v1/router.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
-from . import routes_test, accounts, transactions
+from . import accounts, transactions
 
 router = APIRouter(prefix="/v1")
 
-router.include_router(routes_test.router)
 router.include_router(accounts.router)
 router.include_router(transactions.router)
